50d25869cf199e1bac013bbf51accc12f9b9a760
Ravi Raj <103103100+raviraj3214@users.noreply.github.com>
2024-11-02 21:50:25 +0530

    frontend url update

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(cookieParser())
 app.use(morgan('dev'));
 app.use(cors({
-  origin: 'http://localhost:5173', 
+  origin: 'https://raviprojectmanager.netlify.app', 
   credentials: true      
 }));
 app.use(express.json());
